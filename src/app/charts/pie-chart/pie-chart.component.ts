9e668c7a71631236d5e7e1896e444ea14ae749ef
Abid <abid.karac@edu.fit.ba>
2019-07-10 20:34:37 +0200

    Added pie chart component

--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -9,6 +9,16 @@ export class PieChartComponent implements OnInit {
 
   constructor() { }
 
+  pieChartData:number[] = [350, 450, 120];
+  pieChartLabels: string[] = ['XYZ hosting', 'nSoft', 'Authority Partners'];
+  public pieChartColors = [
+    {
+      backgroundColor: ['#4682B4', '#FFB6C1', '#66CDAA'],
+      borderColor: '#111'
+    },
+  ];
+  pieChartType = 'pie'
+
   ngOnInit() {
   }
 
