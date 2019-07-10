6cd519eca1bbb3dc87291324acd4be1c29a1c218
Abid <abid.karac@edu.fit.ba>
2019-07-10 21:20:12 +0200

    Created line-chart

--- src/app/charts/bar-chart/bar-chart.component.ts
+++ src/app/charts/bar-chart/bar-chart.component.ts
@@ -1,11 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
-const SAMPLE_BARCHART_DATA: any[] = [
-  { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-  { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
-];
-
-const SAMPLE_BARCHART_LABLES: string[] = ['W1','W2','W3','W4','W5','W6','W7'];
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-bar-chart',
@@ -16,8 +11,11 @@ export class BarChartComponent implements OnInit {
 
   constructor() { }
 
-  public barChartData: any[] = SAMPLE_BARCHART_DATA;
-  public barChartLables: string[] = SAMPLE_BARCHART_LABLES;
+  public barChartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+  ];
+  public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
   public barChartType = 'bar';
   public barChartLegend = true;
   public barChartOptions: any = {
--- src/app/charts/line-chart/line-chart.component.ts
+++ src/app/charts/line-chart/line-chart.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { LINECHARTCOLORS } from '../../shared/chartColors';
 
+const LINECHARTSAMPLEDATA: any[] = [
+  { data: [788, 611, 322, 81, 56, 55, 40], label: 'Series A' },
+  { data: [28, 48, 345, 855, 86, 27, 374], label: 'Series B' },
+  { data: [180, 480, 770, 90, 1000, 270, 400], label: 'Series C' }
+]
+
+const LINECHARTLABELS: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
 @Component({
   selector: 'app-line-chart',
   templateUrl: './line-chart.component.html',
@@ -9,7 +17,17 @@ export class LineChartComponent implements OnInit {
 
   constructor() { }
 
+  lineChartData = LINECHARTSAMPLEDATA;
+  lineChartLabels = LINECHARTLABELS;
+  lineChartLegend:true;
+  lineChartOptions:any = {
+    responsive:true
+  };
+  lineChartType = 'line';
+  lineChartColors = LINECHARTCOLORS;
+
   ngOnInit() {
+
   }
 
 }
